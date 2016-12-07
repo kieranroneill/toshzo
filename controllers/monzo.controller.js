abecc968834fc3791e4eecf78ddc4eb83c6f93fc
Kieran O'Neill <kieranroneill@googlemail.com>
2016-12-07 21:31:20 +0000

    Adds function to check webhooks and unit tests

--- controllers/monzo.controller.js
+++ controllers/monzo.controller.js
@@ -11,6 +11,40 @@ const config = require('../config/default.json');
 const errors = require('../config/errors.json');
 
 module.exports = {
+    getWebhooks: (accountIds) => {
+        const deferred = Q.defer();
+        const options = {
+            url: config.MONZO.BASE + config.MONZO.WEBHOOKS,
+            headers: {
+                Authorization: process.env.MONZO_ACCESS_TOKEN
+            },
+            json: true
+        };
+
+        requestClient
+            .get(options, (error, response, body) => {
+                if(error || response.statusCode !== httpCodes.OK) {
+                    return deferred.reject(util.createError(response.statusCode, [errors.INVALID_MONZO_TOKEN]));
+                }
+
+                deferred.resolve(body.webhooks);
+            });
+
+        // Remove any irrelevant webhooks.
+        return deferred
+            .promise
+            .then(webhooks => {
+                return _.filter(webhooks, webhook => {
+                    return ((accountIds.indexOf(webhook.account_id) >= 0) &&
+                    (webhook.url === process.env.MONZO_WEBHOOK_URL));
+                });
+            });
+    },
+
+    // registerWebhooks: () => {
+    //
+    // },
+
     getAccounts: () => {
         const deferred = Q.defer();
         const options = {
--- test/controllers/monzo.test.js
+++ test/controllers/monzo.test.js
@@ -12,12 +12,104 @@ const errors = require('../../config/errors.json');
 const expect = chai.expect;
 
 describe('controllers/monzo', () => {
+    const accountId = 'I am Groot';
+    const anotherAccountId = 'I am baby Groot';
+
     beforeEach(() => {
         this.requestClientGetStub = sinon.stub(requestClient, 'get');
     });
 
     afterEach(() => {
         this.requestClientGetStub.restore();
+    });
+
+    describe('checkWebhooks()', () => {
+        it('should fail if the access token is invalid', done => {
+            const responseBody = {
+                code: 'unauthorized',
+                message: 'User authentication required'
+            };
+
+            process.env.MONZO_ACCESS_TOKEN = 'So terribly invalid.';
+
+            this.requestClientGetStub
+                .callsArgWith(1, null, { statusCode: httpCodes.UNAUTHORIZED, body: responseBody }, responseBody);
+
+            monzoController
+                .getWebhooks([accountId])
+                .catch(error => {
+                    expect(error).to.be.an('object');
+                    expect(error).to.have.property('status')
+                        .to.equal(httpCodes.UNAUTHORIZED);
+                    expect(error).to.have.property('error')
+                        .to.be.an('array')
+                        .to.include(errors.INVALID_MONZO_TOKEN);
+
+                    done();
+                });
+        });
+
+        it('should provide a list of filtered webhooks', done => {
+            const responseBody = {
+                webhooks: [
+                    {
+                        account_id: accountId,
+                        id: 'webhook_000091yhhOmrXQaVZ1Irsv',
+                        url: process.env.MONZO_WEBHOOK_URL
+                    },
+                    {
+                        account_id: 'I am Rocket',
+                        id: 'webhook_99978SkdgsAsta7stiad77',
+                        url: 'http://no.idea.where.com'
+                    }
+                ]
+            };
+
+            process.env.MONZO_ACCESS_TOKEN = 'I am a valid token.... ftw!';
+
+            this.requestClientGetStub
+                .callsArgWith(1, null, { statusCode: httpCodes.OK, body: responseBody }, responseBody);
+
+            monzoController
+                .getWebhooks([accountId])
+                .then(result => {
+                    expect(result).to.be.an('array');
+                    expect(result).to.be.lengthOf(1);
+
+                    done();
+                });
+        });
+
+        it('should provide the list of webhooks', done => {
+            const responseBody = {
+                webhooks: [
+                    {
+                        account_id: accountId,
+                        id: 'webhook_000091yhhOmrXQaVZ1Irsv',
+                        url: process.env.MONZO_WEBHOOK_URL
+                    },
+                    {
+                        account_id: anotherAccountId,
+                        id: 'webhook_99978SkdgsAsta7stiad77',
+                        url: process.env.MONZO_WEBHOOK_URL
+                    }
+                ]
+            };
+
+            process.env.MONZO_ACCESS_TOKEN = 'I am a valid token.... ftw!';
+
+            this.requestClientGetStub
+                .callsArgWith(1, null, { statusCode: httpCodes.OK, body: responseBody }, responseBody);
+
+            monzoController
+                .getWebhooks([accountId, anotherAccountId])
+                .then(result => {
+                    expect(result).to.be.an('array');
+                    expect(result).to.be.lengthOf(2);
+
+                    done();
+                });
+        });
     });
 
     describe('getAccounts()', () => {
