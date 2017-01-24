07924d215366408a1f6a872e32e6cd0562fd431e
Kieran O'Neill <kieranroneill@googlemail.com>
2017-01-24 12:50:41 +0000

    Adds test for Toshzo drawer

---
+++ public/src/components/ToshzoDrawer/ToshzoDrawer.spec.jsx
@@ -0,0 +1,51 @@
+// Components.
+import { ToshzoDrawerTest } from './ToshzoDrawer';
+
+// ActionCreators
+import { ConfigActionCreators } from '../../action-creators/index';
+
+// Store.
+import { ConfigState as initialConfigState } from '../../states/index';
+
+import { getDefaultProps, shallowWithContext } from '../../test/utilities';
+
+describe('<ToshzoDrawer />', () => {
+    beforeEach(function() {
+        this.props = getDefaultProps();
+    });
+
+    afterEach(function() {
+        delete this.props;
+    });
+
+    describe('when setting the drawer open/close state', function() {
+        it('should be set to close for the initial state', function() {
+            const wrapper = shallowWithContext(<ToshzoDrawerTest { ...this.props } />);
+            const instance = wrapper.instance();
+
+            expect(instance.props.config.isDrawerOpen).to.equal(initialConfigState.isDrawerOpen);
+            expect(wrapper.props().open).to.equal(initialConfigState.isDrawerOpen);
+        });
+
+        it('should be open if the state configuration is true', function() {
+            let wrapper;
+
+            this.props.config.isDrawerOpen = true;
+
+            wrapper = shallowWithContext(<ToshzoDrawerTest { ...this.props } />);
+
+            expect(wrapper.props().open).to.be.true;
+        });
+    });
+
+    describe('when clicking the close navigation button', function() {
+        it('should close the navigation drawer', function() {
+            const instance = shallowWithContext(<ToshzoDrawerTest { ...this.props } />)
+                .instance();
+
+            instance.onNavigationCloseClick();
+
+            assert.calledWith(instance.props.dispatch, ConfigActionCreators.toggleDrawer());
+        });
+    });
+});
