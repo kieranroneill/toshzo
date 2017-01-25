e92354bb8288d0360242ec5cc4e367e4e8adcfe3
Kieran O'Neill <kieranroneill@googlemail.com>
2017-01-25 14:42:48 +0000

    Adds info route

---
+++ test/integration/info.test.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const packageJson = require('../../package.json');
+
+const route = config.ENDPOINTS.API + config.ENDPOINTS.INFO;
+
+describe('/info', () => {
+    before(function() {
+        this.app = server.app;
+    });
+
+    describe('get the information', function() {
+        it('should get the information', function(done) {
+            supertest(this.app)
+                .get(route)
+                .expect(httpCodes.OK)
+                .end((error, response) => {
+                    expect(error).to.equal(null);
+                    expect(response.body).to.be.an('object');
+
+                    expect(response.body).to.have.property('author');
+                    expect(response.body.author).to.equal(packageJson.author.name);
+
+                    expect(response.body).to.have.property('description');
+                    expect(response.body.description).to.equal(packageJson.description);
+
+                    expect(response.body).to.have.property('source');
+                    expect(response.body.source).to.equal(packageJson.repository.url);
+
+                    expect(response.body).to.have.property('version');
+                    expect(response.body.version).to.equal(packageJson.version);
+
+                    done();
+                });
+        });
+    });
+});
