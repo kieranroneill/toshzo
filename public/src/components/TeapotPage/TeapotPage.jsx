944be1b5587623c14b1a942069317343c7ae841f
Kieran O'Neill <kieranroneill@googlemail.com>
2017-01-26 19:54:51 +0000

    Adds teapot page component and route

---
+++ public/src/components/TeapotPage/TeapotPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class TeapotPage extends React.Component {
+    render() {
+        return (
+            <div>I am a teapot!!!!</div>
+        );
+    }
+}
+
+export default TeapotPage;
