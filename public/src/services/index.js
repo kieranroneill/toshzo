a57d7143deb8eaee957fceaea773825b3c139c00
Kieran O'Neill <kieranroneill@googlemail.com>
2017-01-26 15:35:45 +0000

    Adds client side services for session tokens

--- public/src/services/index.js
+++ public/src/services/index.js
@@ -1,4 +1,5 @@
 export InfoService from './InfoService';
 export MonzoService from './MonzoService';
 export ReferencesService from './ReferencesService';
+export SessionService from './SessionService';
 export ToshlService from './ToshlService';
