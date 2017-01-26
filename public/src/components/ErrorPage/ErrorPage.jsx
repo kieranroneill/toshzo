1db917c7efeef21cbe924a7a1ff4c0a45f43ae26
Kieran O'Neill <kieranroneill@googlemail.com>
2017-01-26 12:17:35 +0000

    Adds error page template and updates route tests to handle errors

---
+++ public/src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './ErrorPage.scss';
+
+class ErrorPage extends React.Component {
+    render() {
+        return (
+            <div>Uh Oh...</div>
+        );
+    }
+}
+
+export default ErrorPage;
